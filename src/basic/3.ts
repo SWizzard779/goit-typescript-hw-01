a33ec45fde32d8a272a6482335fec43d83e71ea0
SOFIA <sofia.nesimko@gmail.com>
2024-06-19 16:36:08 +0200

    basic part

---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+let mixed: string | number;
+
+type switcher = "enable" | "disable";
+let value: switcher;
+value = "disable"
---
+++ src/basic/5.ts
@@ -0,0 +1,17 @@
+enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+    
+    
+const isWeekend = (day: DayOfWeek): boolean => {
+const weekend: boolean = day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+return weekend;
+}
+console.log(isWeekend(DayOfWeek.Saturday));
+console.log(isWeekend(DayOfWeek.Tuesday));
